d3b499433a0501bd15395bb4157f4273e71448dd
ecruzx01 <cafeexpreso@gmail.com>
2026-02-23 19:02:00 -0400

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,47 +1,25 @@
-const CACHE_NAME = 'maxrewards-v1';
+const CACHE_NAME = 'cardmax-cache-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/438/438526.png'
 ];
 
-// Install Event
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
         return cache.addAll(ASSETS_TO_CACHE);
       })
   );
 });
 
-// Fetch Event - Serve from Cache, Fallback to Network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Return cached version if found
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+        // Return cached asset if found, otherwise fetch from network
+        return response || fetch(event.request);
       })
-  );
-});
-
-// Activate Event - Clean up old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
